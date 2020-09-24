4e9779b0aba8e410b7fc76186fd9b66e4aea2e20
Jonatan <Jonatandahl99@gmail.com>
2020-09-24 17:17:57 +0200

    Added activites, and also changed the style of the app, to remove titelbar.

--- app/src/main/java/com/example/madbudget/MainActivity.kt
+++ app/src/main/java/com/example/madbudget/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.madbudget
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,11 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recipes.setOnClickListener({
-            // Do stuff
-        })
-        createRecipes.setOnClickListener({
-            // Do other stuff
-        })
+        recipes.setOnClickListener{
+            val recipesActivity = Intent(this, Recipes::class.java)
+            startActivity(recipesActivity);
+        }
+        createRecipes.setOnClickListener{
+            val createRecipesActivity = Intent(this, CreateRecipes::class.java)
+            startActivity(createRecipesActivity)
+        }
     }
 }
