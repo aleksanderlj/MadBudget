8abf6cdd25efbf6b0f1023c462e8c068aed24bfc
Jonatan <Jonatandahl99@gmail.com>
2020-10-01 14:13:50 +0200

    Begun the work on the recipe recyclerview, and created dummydata for the recyclerview.

--- app/src/main/java/com/example/madbudget/Recipes.kt
+++ app/src/main/java/com/example/madbudget/Recipes.kt
@@ -3,15 +3,17 @@ package com.example.madbudget
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.madbudget.models.Ingredient
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.madbudget.models.Recipe
 
 class Recipes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipes)
 
-
-
-
-
+        val recipeList: ArrayList<Recipe> = ArrayList()
+        val ingredientList: ArrayList<Ingredient> = ArrayList()
+        ingredientList.add( Ingredient("Rice", 2.7, 5, "Fisk"))
+        recipeList.add(Recipe("Hej", "42069", 10, "2h", ingredientList))
     }
 }
---
+++ app/src/main/java/com/example/madbudget/RecipesAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.madbudget
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madbudget.models.Recipe
+import kotlinx.android.synthetic.main.recipe_item.view.*
+
+class RecipesAdapter(private val myDataset: ArrayList<Recipe>) : RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipesViewHolder {
+        var itemView = LayoutInflater.from(parent.context).inflate(R.layout.recipe_item,parent,false)
+        return RecipesViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
+        val currentItem = myDataset[position]
+        holder.textView.text = currentItem.recipeName
+    }
+
+    override fun getItemCount(): Int {
+        return myDataset.size
+    }
+
+    class RecipesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var textView: TextView = itemView.recipe_name
+    }
+}
