c8ceb4577f0d0e9e389baa73da3357a60a3d8362
Jonatan <Jonatandahl99@gmail.com>
2020-10-01 14:22:35 +0200

    Begun the work on the recipe recyclerview, and created dummydata for the recyclerview. more

--- app/src/main/java/com/example/madbudget/Recipes.kt
+++ app/src/main/java/com/example/madbudget/Recipes.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.madbudget.models.Ingredient
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.madbudget.models.Recipe
+import kotlinx.android.synthetic.main.activity_recipes.*
 
 class Recipes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,9 @@ class Recipes : AppCompatActivity() {
         val ingredientList: ArrayList<Ingredient> = ArrayList()
         ingredientList.add( Ingredient("Rice", 2.7, 5, "Fisk"))
         recipeList.add(Recipe("Hej", "42069", 10, "2h", ingredientList))
+
+        recipe_list.adapter = RecipesAdapter(recipeList)
+        recipe_list.layoutManager = LinearLayoutManager(this)
+
     }
 }
