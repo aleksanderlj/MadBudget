f15ea3e80231243046c7ddc5f57315473477a592
aleksanderlj <aleksanderlj@hotmail.com>
2020-10-06 20:22:18 +0200

    Volley kan nu sende et request og få svar

--- app/src/main/java/com/example/madbudget/salling/SallingCommunicator.kt
+++ app/src/main/java/com/example/madbudget/salling/SallingCommunicator.kt
@@ -5,10 +5,12 @@ import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.Task
 
 class SallingCommunicator {
@@ -17,21 +19,28 @@ class SallingCommunicator {
     companion object {
         fun getAllNearbyStores(context: Context, radius: Int) {
             var fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+            var locationCallback = LocationCallback()
 
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(context as AppCompatActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
                 return
             }
 
+            // TODO move location updates to the current activity instead of recreating on each function call.
+            fusedLocationClient.requestLocationUpdates(
+                createLocationRequest(context),
+                locationCallback,
+                Looper.getMainLooper()
+            )
+
             fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
                 Log.i("Location Info", "/v2/stores/?geo=${location?.latitude},${location?.longitude}&radius=$radius")
                 VolleyGetter.send(context, "/v2/stores/?geo=${location?.latitude},${location?.longitude}&radius=$radius")
             }
 
-            fusedLocationClient.requestLocationUpdates()
         }
 
-        fun createLocationRequest(context: Context) {
+        fun createLocationRequest(context: Context): LocationRequest? {
             val locationRequest = LocationRequest.create().apply {
                 interval = 10000
                 priority = LocationRequest.PRIORITY_HIGH_ACCURACY
@@ -41,7 +50,7 @@ class SallingCommunicator {
             val client: SettingsClient = LocationServices.getSettingsClient(context)
             val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
 
-
+            return locationRequest
         }
 
 
