6cb1534474be621b78a127363c698c8211757efa
Jonatan <Jonatandahl99@gmail.com>
2020-11-05 15:44:53 +0100

    Added Sentry to automatically report crashes.

--- app/src/main/java/com/example/madbudget/MainActivity.kt
+++ app/src/main/java/com/example/madbudget/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.madbudget.salling.ActivitySallingTest
+import io.sentry.Sentry
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
