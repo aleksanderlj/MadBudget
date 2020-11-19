823f2a5081780bfb8443ddac9de04c4fe6e42fcd
aleksanderlj <aleksanderlj@hotmail.com>
2020-11-19 15:15:50 +0100

    The big refacturu

--- library/src/main/java/com/androidbuts/multispinnerfilter/MultiSpinnerListener.java
+++
@@ -1,7 +0,0 @@
-package com.androidbuts.multispinnerfilter;
-
-import java.util.List;
-
-public interface MultiSpinnerListener {
-	void onItemsSelected(List<KeyPairBoolData> selectedItems);
-}
--- library/src/main/java/com/androidbuts/multispinnerfilter/SingleSpinnerListener.java
+++
@@ -1,6 +0,0 @@
-package com.androidbuts.multispinnerfilter;
-
-public interface SingleSpinnerListener {
-    void onItemsSelected(KeyPairBoolData selectedItem);
-    void onClear();
-}
